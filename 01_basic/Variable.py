9fdfbdffd16796b32a43fde89b5cecbff984d44b
kunal <97289128+Kunalmarathe@users.noreply.github.com>
2024-04-07 04:58:13 +0000

    variable

---
+++ 01_basic/Variable.py
@@ -0,0 +1,19 @@
+No = 11
+print("Value of No is", No)
+print(type(No))
+
+X = "Marvellous"
+print(X)
+print(type(X)) # <class 'str'>
+
+Marks = 90.67
+print(Marks)
+print(type(Marks)) # <class 'float'>
+
+Y = 6j
+print(Y)
+print(type(Y)) # <class 'complex'>
+
+A = False # first letter is capital
+print(A)
+print(type(A))
---
+++ 01_basic/VariableX.py
@@ -0,0 +1,24 @@
+X = 11
+print(type(X))
+
+X = 11.8
+print(type(X))
+
+X = "Python"
+print(type(X))
+
+X = False
+print(type(X))
+
+X = 11j
+print(type(X))
+
+A = B = C = 11
+print(A)
+print(B)
+print(C)
+
+P, Q, R = 10, 20, 30
+print(P)
+print(Q)
+print(R)
