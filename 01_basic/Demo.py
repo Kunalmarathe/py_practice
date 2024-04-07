aafa5df1776fad63e4b0439eb973faa8c3813b60
kunal <97289128+Kunalmarathe@users.noreply.github.com>
2024-04-07 04:24:16 +0000

    demo

---
+++ 01_basic/Demo.py
@@ -0,0 +1,12 @@
+print("First Program of Python...")
+
+# To compile the python code
+# python Demo.py
+
+# To create .pyc file
+# python -m compileall Demo.py
+# It cteates .pyc file in __pycache__ folder
+# To execute the file
+# python Demo.cpython-310.pyc
+
+# python is dynamically type programming language
