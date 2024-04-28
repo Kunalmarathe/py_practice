98801ddf517cc251715456c1b8dfe8dd20b4dd1e
kunal <97289128+Kunalmarathe@users.noreply.github.com>
2024-04-28 06:13:12 +0000

    Multithreading

---
+++ 01_basic/Multi2.py
@@ -0,0 +1,27 @@
+import os
+import multiprocessing
+
+def Task1(No):
+    print("Executing first task")
+    print("PID of task1: ",os.getpid())
+    print("PPID of task1: ",os.getpid())
+
+def Task2(No):
+    print("Executing first task")
+    print("PID of task2: ",os.getpid())
+    print("PPID of task2: ",os.getpid())
+
+def main():
+    print("PID of running process : ",os.getpid())
+    print("PID of parent process ie command prompt is : ",os.getppid())
+
+    Value = 11
+    p1 = multiprocessing.Process(target = Task1, args = (Value,))
+    p2 = multiprocessing.Process(target = Task1, args = (Value,))
+
+    p1.start()
+    p2.start()
+
+
+if __name__ == "__main__":
+    main()
